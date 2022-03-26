7860eaf9a0719a5ae97158e9e0de378e2a7cd939
sanchesTM <thiago.missfeldt@gmail.com>
2022-03-27 00:34:18 +0800

    Update setup.py
    update description

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     url='https://github.com/sanchestm/regex-genome-tokenizer',
     license='MIT',
     py_modules=['regexTokenizer'],
+    long_description_content_type = 'Package to tokenize genomic data and convert fasta/q files to pandas dataframe',
     #packages=[''],
     install_requires=['numpy','scipy','ipywidgets','scikit-learn','seaborn','matplotlib','pandas','statsmodels'],
 )
